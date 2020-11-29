7a578c166b10d394448955a32bae85c1b69afeff
version-1 <j.onodera26@gmail.com>
2020-11-29 13:19:47 -0800

    add testcase for mailer

--- spec/mailers/previews/users/birthday_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/users/birthday_mailer
-class Users::BirthdayMailerPreview < ActionMailer::Preview
-
-end
--- spec/mailers/users/birthday_mailer_spec.rb
+++ spec/mailers/users/birthday_mailer_spec.rb
@@ -1,5 +1,33 @@
 require "rails_helper"
 
 RSpec.describe Users::BirthdayMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#notify' do
+    let(:locale) { :en }
+    let(:user) { create(:user, locale: locale) }
+    subject { described_class.notify(user.id) }
+
+    it do
+      expect(subject.subject).to eq("Happy Birthday, #{user.preferred_name}")
+      expect(subject.to).to match([user.email])
+    end
+
+    context 'locale other than en' do
+      context 'locale is ja' do
+        let(:locale) { :ja }
+        it do
+          expect(subject.subject).to eq("#{user.preferred_name}さん、誕生日おめでとう!")
+          expect(subject.to).to match([user.email])
+        end
+      end
+
+      context 'locale is fr' do
+        let(:locale) { :fr }
+        it do
+          expect(subject.subject).to eq("Joyeux anniversaire, #{user.preferred_name}!")
+          expect(subject.to).to match([user.email])
+        end
+      end
+    end
+  end
 end
