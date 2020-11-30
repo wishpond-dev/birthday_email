f50d33b35c6fb37aad63b66a0741c39882389ea2
version-1 <j.onodera26@gmail.com>
2020-11-29 16:15:10 -0800

    fix sidekiq setting

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379', namespace: 'test_sidekiq' }
+  config.redis = { url: "redis://#{Settings.redis.host}:#{Settings.redis.port}", namespace: Settings.redis.namespace }
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379', namespace: 'test_sidekiq' }
+  config.redis = { url: "redis://#{Settings.redis.host}:#{Settings.redis.port}", namespace: Settings.redis.namespace }
 end
