c12b15ae993038c57c570f8255d5d9aad987046a
Paulo Borba <pauloesb@gmail.com>
2020-12-04 16:32:43 -0300

    Create user mailer with birthday_email delivery

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+  def birthday_email
+    @user = params[:user]
+
+    I18n.with_locale(@user.locale) do
+      mail(
+        to: @user.email,
+        subject: I18n.t('user_mailer.birthday_email.subject')
+      )
+    end
+  end
+end
