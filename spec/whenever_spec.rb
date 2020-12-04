3ab021b3aaeefa67f2d4f53081f505818e954d74
Paulo Borba <pauloesb@gmail.com>
2020-12-04 18:32:53 -0300

    Create test for whenever scheduler

---
+++ spec/whenever_spec.rb
@@ -0,0 +1,16 @@
+# spec/whenever_spec.rb
+require 'rails_helper'
+
+describe 'Whenever Schedule' do
+  before do
+    load 'Rakefile'
+  end
+
+  it 'makes sure `rake` statements exist and it is the send_birthday_emails:run' do
+    schedule = Whenever::Test::Schedule.new
+
+    assert Rake::Task.task_defined?(schedule.jobs[:rake].first[:task])
+
+    expect(schedule.jobs[:rake].first[:task]).to eq('send_birthday_emails:run')
+  end
+end
