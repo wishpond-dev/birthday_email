881564e55f15aa5d1883ee86b55a2bdf50f84822
Paulo Borba <pauloesb@gmail.com>
2020-12-04 16:37:50 -0300

    Configure shell script to set up the cronjobs using whenever

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 bundle install --quiet
+[[ "$RAILS_ENV" = "" ]] && CURRENT_ENV="development" || CURRENT_ENV="$RAILS_ENV"
+bundle exec whenever --update-crontab --set environment=$CURRENT_ENV
+crond start
 rm -f tmp/pids/server.pid
 rails s -b 0.0.0.0 -p 3000
