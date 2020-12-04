b8a7935ef73c8c24d3a0a692280b28da698d44f5
Paulo Borba <pauloesb@gmail.com>
2020-12-04 17:42:54 -0300

    Adjust start sh to have two executions accoding to env variable

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 bundle install --quiet
-[[ "$RAILS_ENV" = "" ]] && CURRENT_ENV="development" || CURRENT_ENV="$RAILS_ENV"
-bundle exec whenever --update-crontab --set environment=$CURRENT_ENV
-crond start
-rm -f tmp/pids/server.pid
-rails s -b 0.0.0.0 -p 3000
+
+if [ "$SERVICE_TYPE" = "worker" ]; then
+  [[ "$RAILS_ENV" = "" ]] && CURRENT_ENV="development" || CURRENT_ENV="$RAILS_ENV"
+  bundle exec sidekiq -C config/sidekiq.yml
+  bundle exec whenever --update-crontab --set environment=$CURRENT_ENV
+  crond start
+else
+  rm -f tmp/pids/server.pid
+  rails s -b 0.0.0.0 -p 3000
+fi
