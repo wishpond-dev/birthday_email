d1c0f3a4654dff89dc12076f8c214656d24506b0
Paulo Borba <pauloesb@gmail.com>
2020-12-05 07:28:30 -0300

    Add edge case test to send_birthday_emails:run

--- spec/tasks/send_birthday_emails_spec.rb
+++ spec/tasks/send_birthday_emails_spec.rb
@@ -6,13 +6,25 @@ require "rails_helper"
 Rails.application.load_tasks
 
 describe "send_birthday_emails:run" do
-  let!(:user) { create(:user, birthdate: Time.zone.now) }
-  let!(:consent) { create(:consent, key: "email", users: [user]) }
+  context 'when does it have emails to send' do
+    let!(:user) { create(:user, birthdate: Time.zone.now) }
+    let!(:consent) { create(:consent, key: "email", users: [user]) }
 
-  it "expected to fetch the birthdays of today and send email to them" do
-    expect { run_task(task_name: "send_birthday_emails:run") }.to have_enqueued_job
-      .on_queue("mailers")
-      .with("UserMailer", "birthday_email", "deliver_now", user: user)
+    it "expected to fetch the birthdays of today and send email to them" do
+      expect { run_task(task_name: "send_birthday_emails:run") }.to have_enqueued_job
+        .on_queue("mailers")
+        .with("UserMailer", "birthday_email", "deliver_now", user: user)
+    end
+  end
+
+  context 'when does not have emails to send' do
+    let!(:user) { create(:user, birthdate: Time.zone.now + 1.day) }
+    let!(:consent) { create(:consent, key: "email", users: [user]) }
+
+    it "expected to not send any emails" do
+      expect { run_task(task_name: "send_birthday_emails:run") }.not_to have_enqueued_job
+        .on_queue("mailers")
+    end
   end
 end
 
