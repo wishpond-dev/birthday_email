7282658aa2a4b1cc6bcd0b6980c9f2b8ee078906
Paulo Borba <pauloesb@gmail.com>
2020-12-05 07:23:18 -0300

    Fix test send_birthday_emails after rubocop fix

--- spec/tasks/send_birthday_emails_spec.rb
+++ spec/tasks/send_birthday_emails_spec.rb
@@ -6,8 +6,8 @@ require "rails_helper"
 Rails.application.load_tasks
 
 describe "send_birthday_emails:run" do
-  let(:user) { create(:user, birthdate: Time.zone.now) }
-  let(:consent) { create(:consent, key: "email", users: [user]) }
+  let!(:user) { create(:user, birthdate: Time.zone.now) }
+  let!(:consent) { create(:consent, key: "email", users: [user]) }
 
   it "expected to fetch the birthdays of today and send email to them" do
     expect { run_task(task_name: "send_birthday_emails:run") }.to have_enqueued_job
