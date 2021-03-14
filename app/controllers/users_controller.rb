2dd09850377788b0a41a5cec40ab6065862eb228
Raphael Barreto <rfsbarreto@gmail.com>
2021-03-14 14:25:58 -0300

    fix bug on user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,6 @@ class UsersController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def user_params
-    params.require(:user).permit(:encrypted_password, :username, :encrypted_email_iv, :encrypted_email)
+    params.require(:user).permit(:encrypted_password, :username, :encrypted_email_iv, :encrypted_email, :birthdate)
   end
 end
