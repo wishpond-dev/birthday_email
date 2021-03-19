1ebd1db2d76fb5c9ee057df17987846ced519465
Usmanriaz665 <mohammadusmanriaz@gmail.com>
2021-03-19 17:13:29 -0400

    updated mailer translation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
     def birthday_wish
         @user = params[:user]
-        mail(to: @user.email, subject: 'Birthday wish')
+        mail(to: @user.email, subject: default_i18n_subject(user: @user.name))
     end
 end
