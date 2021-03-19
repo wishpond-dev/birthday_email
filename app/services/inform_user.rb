ac78dbd15d7e180ede2ed6405b2fac5477eb7e39
Usmanriaz665 <mohammadusmanriaz@gmail.com>
2021-03-19 17:24:44 -0400

    updated I18 defualt lang

--- app/services/inform_user.rb
+++ app/services/inform_user.rb
@@ -4,6 +4,7 @@ class InformUser
              sleep(50) # Make sure it doesn't get too crowded in there!
             users = group.users
             users.each do |user| 
+                I18n.default_locale = user.locale if user.locale.present?
                 if user.birthday.to_date == Date.today # find today is birthday
                     UserMailer.with(user: user).birthday_wish.deliver_now  
                 end
