066666fd3a6847f43bafae226dda58e5e56cff74
Usmanriaz665 <mohammadusmanriaz@gmail.com>
2021-03-19 18:22:53 -0400

    updated the job

--- app/services/inform_user.rb
+++ app/services/inform_user.rb
@@ -1,7 +1,8 @@
 class InformUser
     def send_email
+        puts 'job starting...'
         Consent.find_by(key: 'email').find_in_batches do |group|
-             sleep(50) # Make sure it doesn't get too crowded in there!
+            sleep(50) # Make sure it doesn't get too crowded in there!
             users = group.users
             users.each do |user| 
                 I18n.default_locale = user.locale if user.locale.present?
@@ -9,6 +10,7 @@ class InformUser
                     UserMailer.with(user: user).birthday_wish.deliver_now  
                 end
             end
-        end    
+        end 
+        puts 'job ends...'   
     end
 end
