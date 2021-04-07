0976898fa3e1a05c34cc34664ef66fcdd0d9ebd9
Fernando Serapio <fhernandosc@gmail.com>
2021-04-07 00:45:58 -0500

    random locale

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,6 @@ Consent.first_or_create(key: 'email')
     user.birthdate = Faker::Date.between_except(from: 30.year.ago, to: 1.day.ago, excepted: Date.today)
   end
   user.consents << Consent.last if i.odd?
+  user.locale = [:en, :es, :fr].sample
   user.save
 end
